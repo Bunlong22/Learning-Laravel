70e5e5f679a08b5948e82b31cee7d45a5660ab53
bunlong22 <suonbunlong93@gmail.com>
2023-07-29 14:19:46 +0700

    add product model

--- routes/web.php
+++ routes/web.php
@@ -68,3 +68,11 @@ Route::put("/category/{categoryId}", [CategoryController::class, 'update'])->nam
 
 Route::delete("/category/{categoryId}", [CategoryController::class, 'destroy'])->name('category.delete');
 Route::get('/category/{cateId}', [CategoryController::class, 'show'])->name("category.show");
+
+Route::get('/product',[ProductController::class,'index'])->name('product.index');
+Route::get('/product/create',[ProductController::class,'create'])->name('product.create');
+Route::post('/product',[ProductController::class,'store'])->name('product.store');
+Route::get('/product/{product}',[ProductController::class,'show'])->name('product.show');
+Route::delete('/product/{product}',[ProductController::class,'destroy'])->name('product.destroy');
+Route::get('/product/{product}/edit',[ProductController::class,'edit'])->name('product.edit');
+Route::put('/product/{product}',[ProductController::class,'update'])->name('product.update');

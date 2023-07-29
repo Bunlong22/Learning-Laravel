2cf5c952ee93e19bc58f39d3863735983956abc3
bunlong22 <suonbunlong93@gmail.com>
2023-07-29 13:59:15 +0700

    add condition if deleted

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -32,9 +32,15 @@
 <button class="btn btn-danger delete">Delete</button>
 {!! Form::close() !!}
                 </td>
+                        
             </tr>	
             @endforeach
-        </tbody>
+        </tbody> @if(Session::has('category_delete'))
+            <div class="alert alert-primary alert-dismissible">
+                <button type="button" class="btn-close" data-bs-dismiss="alert" onclick="if(confirm('Are you sure to delete this book?') === false) event.preventDefault()">></button>
+                <strong>Deleted!</strong> {!! session('category_delete') !!}
+            </div>
+        @endif  
     </table>
 @endif
 @endsection

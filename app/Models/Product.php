b7c9ef709459342e8e0eabb391f50bfb430f2cda
bunlong22 <suonbunlong93@gmail.com>
2023-07-29 14:16:50 +0700

    create product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $table = 'products';
+    protected $fillable = [
+        'name',
+        'price',
+        'description',
+        'image',
+        'category_id'
+    ];
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
